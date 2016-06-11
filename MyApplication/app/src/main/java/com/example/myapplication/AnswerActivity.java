29ccdaadeec7ed408468b33a04f87c2274c54e8c
markmin <942691141@qq.com>
2016-06-11 23:43:38 +0800

    基本界面搭建，准备写一个单词查询的application

---
+++ MyApplication/app/src/main/java/com/example/myapplication/AnswerActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AnswerActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_main);
+
+
+
+    }
+
+
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
+    private TextView wordQuestion;
+    private TextView wordAnswer1;
+    private TextView wordAnswer2;
+    private TextView wordAnswer3;
+    private TextView wordAnswer4;
+    private Button pre;
+    private Button viewAnswer;
+    private Button nextQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+    }
+
+    private void initView() {
+
+
+        wordQuestion = (TextView) findViewById(R.id.word_question);
+        wordAnswer1 = (TextView) findViewById(R.id.word_answer1);
+        wordAnswer2 = (TextView) findViewById(R.id.word_answer2);
+        wordAnswer3 = (TextView) findViewById(R.id.word_answer3);
+        wordAnswer4 = (TextView) findViewById(R.id.word_answer4);
+        pre = (Button) findViewById(R.id.pre);
+        viewAnswer = (Button) findViewById(R.id.view_answer);
+         viewAnswer.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(getApplicationContext(),AnswerActivity.class);
+
+                 startActivity(intent);
+             }
+         });
+        nextQuestion = (Button) findViewById(R.id.next_question);
+
     }
 }
