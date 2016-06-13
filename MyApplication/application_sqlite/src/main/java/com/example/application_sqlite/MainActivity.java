980e97c17db4cee201f18836950f9e3b6f9f0ecf
markmin <942691141@qq.com>
2016-06-14 02:06:27 +0800

    #创建数据库 SQLiteOpenHelper CRUD 操作

--- MyApplication/application_sqlite/src/main/java/com/example/application_sqlite/MainActivity.java
+++ MyApplication/application_sqlite/src/main/java/com/example/application_sqlite/MainActivity.java
@@ -1,26 +1,101 @@
 package com.example.application_sqlite;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     MyDatabaseHelper dbHelper;
+    SQLiteDatabase db;
+    private TextView tvShowContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
          dbHelper = new MyDatabaseHelper(this,"Book.db",null,1);
-        Button tvCreate = (Button) findViewById(R.id.tv_create);
-        tvCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dbHelper.getWritableDatabase();
-            }
-        });
 
+
+        tvShowContent = (TextView) findViewById(R.id.tv_show_content);
+
+
+          findViewById(R.id.tv_create).setOnClickListener(this);
+          findViewById(R.id.tv_insert).setOnClickListener(this);
+          findViewById(R.id.tv_updata).setOnClickListener(this);
+          findViewById(R.id.tv_delete).setOnClickListener(this);
+          findViewById(R.id.tv_query).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        db = dbHelper.getWritableDatabase();
+
+        switch (v.getId()){
+            case R.id.tv_create:
+
+            break;
+            case R.id.tv_insert:
+
+                ContentValues values = new ContentValues();
+                values.put(MyDatabaseHelper.COLUMN_TITLE,"First code");
+                values.put(MyDatabaseHelper.COLUMN_PRICE,"66.8");
+                db.insert(MyDatabaseHelper.TABLE_NAME, null, values);
+                values.clear();
+            break;
+            case R.id.tv_updata:
+
+                ContentValues valueUpdata = new ContentValues();
+                valueUpdata.put(MyDatabaseHelper.COLUMN_PRICE, 88.6);
+                db.update(MyDatabaseHelper.TABLE_NAME, valueUpdata,
+                        MyDatabaseHelper.COLUMN_TITLE + "=?", new String[]{"First code"});
+
+            break;
+            case R.id.tv_delete:
+
+                db.delete(MyDatabaseHelper.TABLE_NAME,
+                        MyDatabaseHelper.COLUMN_PRICE + " > ?", new String[]{"80"});
+            break;
+
+            case R.id.tv_query:
+                getAllData();
+                break;
+
+        }
+    }
+
+    private void getAllData() {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                MyDatabaseHelper.COLUMN_ENTRY_ID,
+                MyDatabaseHelper.COLUMN_TITLE,
+                MyDatabaseHelper.COLUMN_PRICE
+        };
+
+        Cursor c = db.query(
+                MyDatabaseHelper.TABLE_NAME, projection, null, null, null, null, null);
+
+        if (c != null && c.getCount() > 0) {
+            while (c.moveToNext()) {
+                String title = c.getString(c.getColumnIndex(MyDatabaseHelper.COLUMN_TITLE));
+                double price = c.getDouble(c.getColumnIndex(MyDatabaseHelper.COLUMN_PRICE));
+                tvShowContent.append("\n Query "+ title +"  price:"+price);
+
+            }
+        }
+        if (c != null) {
+            c.close();
+        }
+
+        db.close();
     }
 }
--- MyApplication/application_sqlite/src/main/java/com/example/application_sqlite/MyDatabaseHelper.java
+++ MyApplication/application_sqlite/src/main/java/com/example/application_sqlite/MyDatabaseHelper.java
@@ -9,7 +9,33 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
+    public static final String TABLE_NAME = "BOOK";
+    public static final String COLUMN_ENTRY_ID = "entryid";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_CATEGORYID = "cagegoryid";
+    public static final String COLUMN_PRICE = "completed";
+
+    public static final String TABLE_NAME2 = "Category";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_CODE = "code";
+
+
+
     private static final String CREATE_TABLE
+            = "create table "+TABLE_NAME+" ("+
+            COLUMN_ENTRY_ID +" integer primary key autoincrement," +
+            COLUMN_TITLE +" text," +
+            COLUMN_PRICE +" real," +
+            COLUMN_CATEGORYID+" integer)"; // version 3 new add
+
+    private static final String CREATE_TABLE2 //version 2 add
+            = "create table "+TABLE_NAME2+" ("+
+            COLUMN_ENTRY_ID +" integer primary key autoincrement," +
+            COLUMN_NAME+" text," +
+            COLUMN_CODE+" integer)";
+
+
+    private static final String CREATE_TABLE1
             = "create table Book (id integer primary key autoincrement, author text, price real)";
 
 
@@ -22,11 +48,18 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_TABLE2);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        switch (newVersion) {
+            case 1:
+                db.execSQL(CREATE_TABLE2);
+            case 2:
+                db.execSQL("alter table "+TABLE_NAME+"add column "+COLUMN_CATEGORYID+" integer");
+                default:
+        }
     }
 }
