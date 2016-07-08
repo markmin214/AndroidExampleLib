cbd9e8184f98c5206792755038e9b77d1c915cba
markmin <942691141@qq.com>
2016-07-08 23:04:24 +0800

    OnNewArrivedListener

--- MyApplication/app_aidl_sevice/src/main/java/com/example/myapplication/BookManagerService.java
+++ MyApplication/app_aidl_sevice/src/main/java/com/example/myapplication/BookManagerService.java
@@ -7,10 +7,14 @@ import android.os.RemoteException;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BookManagerService extends Service {
 
     private CopyOnWriteArrayList<Book> mBookList = new CopyOnWriteArrayList<Book>();
+    private CopyOnWriteArrayList<IOnNewBookArrivedListener> listeners =
+            new CopyOnWriteArrayList<IOnNewBookArrivedListener>();
+    private AtomicBoolean mIsServiceDestoryed = new AtomicBoolean(false);
 
 
     private final IBookManager.Stub mBinder = new IBookManager.Stub() {
@@ -30,6 +34,20 @@ public class BookManagerService extends Service {
                 }
             }
         }
+
+        @Override
+        public void registerListener(IOnNewBookArrivedListener listener) throws RemoteException {
+            if (!listeners.contains(listener)) {
+                listeners.add(listener);
+            }
+        }
+
+        @Override
+        public void unregisterListener(IOnNewBookArrivedListener listener) throws RemoteException {
+            if (listeners.contains(listener)) {
+                listeners.remove(listener);
+            }
+        }
     };
 
     public BookManagerService() {
@@ -41,10 +59,51 @@ public class BookManagerService extends Service {
         super.onCreate();
         mBookList.add(new Book(1,"Book 1"));
         mBookList.add(new Book(2,"Book 2"));
+        updataBook();
+    }
+
+    private void updataBook() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!mIsServiceDestoryed.get()) {
+                    try {
+                        Thread.sleep(5000);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                int bookid = mBookList.size()+1;
+                    Book newbook = new Book(bookid, "NewBOOK#" + bookid);
+                    try {
+                        onNewBookArrived(newbook);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+    }
+
+    private void onNewBookArrived(Book newbook) throws RemoteException {
+        mBookList.add(newbook);
+        for (int i=0;i<listeners.size();i++) {
+            IOnNewBookArrivedListener listener = listeners.get(i);
+            listener.onNewBookArrived(newbook);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        mIsServiceDestoryed.set(true);
+        super.onDestroy();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return mBinder;
     }
+
+
 }
