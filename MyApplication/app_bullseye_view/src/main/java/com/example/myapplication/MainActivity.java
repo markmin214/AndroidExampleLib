f61fdda4e6d0aac823f2e3e9f695ef0b6308cf49
markmin <942691141@qq.com>
2016-07-11 23:25:36 +0800

    CustomView

---
+++ MyApplication/app_bullseye_view/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_bulls_eye_view);
+    }
+}
